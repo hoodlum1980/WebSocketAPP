d0f245aa8ee914441c16b795ac98e83dca5749ef
mullermiky <muller.miky@gmail.com>
2016-11-22 21:00:30 +0100

    added basic buttons and its function

--- script.js
+++ script.js
@@ -24,12 +24,28 @@ function testWebSocket() {
 
 function onOpen(evt) {
    writeToScreen("CONNECTED");
-   doSend("WebSocket rocks");
+   //doSend("WebSocket rocks");
+}
+
+function clickSend(){
+    var pre = output.getElementsByTagName("p");
+    var inputList = Array.prototype.slice.call(pre);
+    console.log(pre.length);
+
+    for (i = 0; i<=pre.length;i++){
+        console.log(inputList[i]+" deleted "+i);
+        output.removeChild(inputList[i]);
+    }
+    doSend(document.getElementById("inputText").value);
+}
+
+function clickClose(){
+    websocket.close();
 }
 
 function onMessage(evt) {
    writeToScreen('<span style = "color: blue;">RESPONSE: ' +
-      evt.data+'</span>'); websocket.close();
+      evt.data+'</span>');
 }
 
 function onError(evt) {
@@ -37,13 +53,15 @@ function onError(evt) {
 }
 
 function doSend(message) {
-   writeToScreen("SENT: " + message); websocket.send(message);
+   writeToScreen("SENT: " + message);
+   websocket.send(message);
 }
 
 function writeToScreen(message) {
    var pre = document.createElement("p");
    pre.style.wordWrap = "break-word";
-   pre.innerHTML = message; output.appendChild(pre);
+   pre.innerHTML = message;
+   output.appendChild(pre);
 }
 
 window.addEventListener("load", init, false);
