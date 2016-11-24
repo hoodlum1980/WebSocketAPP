7af488a05b24b35e05194067ca1ea36b145c6dcc
mullermiky <muller.miky@gmail.com>
2016-11-24 17:02:40 +0100

    added variable for script name

---
+++ echo_handler.h
@@ -0,0 +1,4 @@
+#ifndef ECHO_HANDLER_H
+#define ECHO_HANDLER_H
+
+#endif // ECHO_HANDLER_H
---
+++ handler.h
@@ -0,0 +1,16 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+
+class handler : public QObject
+{
+    Q_OBJECT
+public:
+    explicit handler(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // HANDLER_H
